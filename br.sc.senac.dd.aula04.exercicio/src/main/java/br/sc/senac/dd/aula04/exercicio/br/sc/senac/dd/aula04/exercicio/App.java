437ebf68208aa19318ebecce87b820ba347e6b14
vincent gabriel maris dafre <vinvinvinvin123@gmail.com>
2018-08-06 19:14:42 -0300

    master

---
+++ br.sc.senac.dd.aula04.exercicio/src/main/java/br/sc/senac/dd/aula04/exercicio/br/sc/senac/dd/aula04/exercicio/App.java
@@ -0,0 +1,13 @@
+package br.sc.senac.dd.aula04.exercicio.br.sc.senac.dd.aula04.exercicio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
